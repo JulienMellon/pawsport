b28bedf15a0583f1941e290c1919cc87eaf36928
Julien Mellon <mellon.julien@gmail.com>
2022-10-02 18:38:25 -0400

    cleaned up some files

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
 const petsController = require("../controllers/pets");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
@@ -10,7 +9,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, petsController.getProfile);
 router.get("/addPet", ensureAuth, petsController.getAddPet)
-router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
-// const commentRoutes = require("./routes/comments");
 const petRoutes = require("./routes/pets");
 const vaccineRoutes = require("./routes/vaccines");
 
@@ -58,8 +56,6 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
-// app.use("/comments", commentRoutes);
 app.use("/pet", petRoutes);
 app.use("/vaccine", vaccineRoutes);
 
